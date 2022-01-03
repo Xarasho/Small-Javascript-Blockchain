d6765b6b2af6a95a19b9ba45a59917413ea701e3
Alex <alex.olmedo.rojas@gmail.com>
2022-01-02 23:08:25 -0600

    Created Blockchain class

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const lightningHash = (data) => {
-    return data + '*';
+  return data + '*';
 }
 
 class Block {
-    constructor(data, hash, lastHash) {
-        this.data = data;
-        this.hash = hash;
-        this.lastHash = lastHash;
-    }
+  constructor(data, hash, lastHash) {
+    this.data = data;
+    this.hash = hash;
+    this.lastHash = lastHash;
+  }
 }
 
-const fooBlock = new Block('foo', 'fooHash', 'fooLastHash');
-console.log(fooBlock);
+class Blockchain {  
+  constructor() {
+      const genesis = new Block('gen-data', 'gen-hash', 'gen-last-hash');
+        
+      this.chain([genesis]);
+    }
+  
+  addBlock(data) {
+      const lastHash = this.chain[this.chain.length - 1].hash;
+
+      const hash = lightningHash(data + lastHash);
+
+      const block = new Block(data, hash, lastHash);
+
+      this.chain.push(block);
+    }
+}
